51374648382a096f3011699a6211d8cbbdb0edb1
kagan <yasirkagang@gmail.com>
2022-05-18 19:29:22 +0300

    18.05.2022

---
+++ src/test/java/day08_alerts/C03_BasicAuthentication.java
@@ -0,0 +1,21 @@
+package day08_alerts;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_BasicAuthentication {
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        // https://the-internet.herokuapp.com/javascript_alerts adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+    }
+}
